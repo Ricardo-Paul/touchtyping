ca23502a5cd337b528aed8d581eeb7c862547107
Ricardo-Paul <ricardopaul459@gmail.com>
2021-05-25 20:20:22 -0400

    handle undegined language

--- main.js
+++ main.js
@@ -10,28 +10,29 @@ function setLanguage(_language = defaultLanguage){
   fetch("data/words.json")
   .then(response => response.json())
   .then(json => {
-
-    // deice what language to set based on cookie
-    // if language not set, set english as the default language for the user
+    if(json[_language] === undefined){
+      console.log(`We currently don't support ${_language}`);
+    };
     randomWords = json[_language].split(" ");
-    console.log("WORDS :", randomWords);
+    console.log(randomWords)
   });
 };
 
-setLanguage();
+setLanguage("english");
 
 
 // set cookie along with an expiry date
 function setCookie(cname, cvalue, exdays){
   let d = new Date();
-  d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
   let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + exdays + expires + ";" + "path=/"
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";" + "path=/"
 };
 
 function getCookie(cname){
-  // let cookies = decodeURIComponent(document.cookie);
-  let c = "timeCount=60; typingMode=wordcount; punctuation=false; _ga=GA1.2.212430062.1621050011; theme=moderndolch; wordCount=25; _gid=GA1.2.2029559042.1621902074; language=english; _gat_gtag_UA_126815322_3=1"
+
+  // Renovie the cookie string
+  let c = document.cookie || "timeCount=60; typingMode=wordcount; punctuation=false; _ga=GA1.2.212430062.1621050011; theme=moderndolch; wordCount=25; _gid=GA1.2.2029559042.1621902074; language=english; _gat_gtag_UA_126815322_3=1"
 
   let cookies = decodeURIComponent(c);
   let ca = cookies.split(";");
